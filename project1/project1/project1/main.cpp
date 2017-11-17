5b42b4cef8e3e3c60b9d978fabdebc06fb3e6628
Recep Deniz Aksoy <recepdeniza@gmail.com>
2017-11-17 18:54:20 +0300

    documentation finished

--- project1/project1/project1/main.cpp
+++ project1/project1/project1/main.cpp
@@ -1,8 +1,20 @@
+/*
+ * Student Name: Recep Deniz Aksoy
+ * Student Number: 2014400150
+ * Project Number: #1
+ */
 #include <iostream>
 #include <fstream>
 #include <cstring>
 #include <queue>
 #include <cstdio>
+
+
+/**
+ * In this project we re asked to implement an OS, starting from 
+ * process scheduler which executes using round robin algorithm.
+ *
+ */
 
 #define QUANT 100 //Time quantum.
 
@@ -12,6 +24,11 @@ using namespace std;
  *  instr -> Holds the instructions' lengths
  *  name -> Name of the process
  *  file -> Name of the code file
+ *
+ * Processes are defined as structs that have name and file name
+ * of the process. Also process' arrival time is stored. An
+ * integer vector holds the all instructions' execution time
+ * consecutive.
  */
 struct Process{
     queue<int> instr;
@@ -24,6 +41,13 @@ struct Process{
         name = n;
     }
 };
+
+/** print_queue function:
+ *  int t -> Time
+ *  queue<Process> ps_queue -> Process queue to be wrote in file.
+ *  ofstream & out -> Output stream for writing in file.
+ *
+ */
 
 void print_queue(int t, queue<Process> ps_queue, ofstream & out){
     out << t << "::HEAD-";
@@ -38,15 +62,26 @@ void print_queue(int t, queue<Process> ps_queue, ofstream & out){
     out << "TAIL" << endl;
 }
 
-/** Round robin scheduler
+/** rr_scheduler function:
+ *  queue<Process> & ps_q -> Initial queue for process' 
+ *                           ordered in arrival times.
+ *
+ *  ofstream & o -> Output stream for writing in file.
+ *                  That will be passed into print_queue
+ *                  function.
+ *
+ *  Basic round robin algorithm. Steps described inline below.
  */
+
 void rr_scheduler(queue<Process> & ps_q, ofstream & o){
     int time = 0;
     queue<Process> ps_queue;
     //Initial process has arrived.
     ps_queue.push(ps_q.front());
+    //Printing the queue in first arrival.
     print_queue(time, ps_queue, o);
     ps_q.pop();
+    //While loop executes until all processes has been processed.
     while (!ps_queue.empty()) {
         int q_count = 0;
         while (q_count < QUANT && !ps_queue.front().instr.empty()) {
@@ -76,6 +111,7 @@ void rr_scheduler(queue<Process> & ps_q, ofstream & o){
 }
 
 int main(int argc, const char * argv[]) {
+    //If arguments not matched throw error.
     if (argc != 3) {
         cout << "Run the code with the following command: ./project1 [input_file] [output_file]" << endl;
         return 1;
@@ -102,6 +138,7 @@ int main(int argc, const char * argv[]) {
         myfile.close();
     }
     ofstream out(argv[2]);
+    //Send process arrival queue to rr_scheduler.
     rr_scheduler(ps_queue, out);
     out.close();
     return 0;
